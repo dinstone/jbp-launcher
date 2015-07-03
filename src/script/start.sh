82c2d230e340f211748d3ce9f0942b94a62fd889
dinstone <dinstone@dinstone-PC>
2015-07-03 17:37:37 +0800

    shell edit

--- src/script/start.sh
+++ src/script/start.sh
@@ -2,18 +2,22 @@
 
 LAUNCHER_HOME=$(cd `dirname $0`; pwd)
 
-#java -server -Xss256k -Xms4g -Xmx4g -Xmn1g -XX:PermSize=128m -XX:MaxPermSize=256m -Djava.net.preferIPv4Stack=true -XX:+DisableExplicitGC -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/jvmlog/jvm.hprof -XX:+PrintClassHistogram -Xloggc:/tmp/gc.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC -cp bin/bootstrap.jar cn.citic21.startup.Bootstrap start > /dev/null  2>&1 &
+#java -server -Xss256k -Xms4g -Xmx4g -Xmn1g -XX:PermSize=128m -XX:MaxPermSize=256m -Djava.net.preferIPv4Stack=true -XX:+DisableExplicitGC -XX:+HeapDumpOnOutOfMemoryError -XX:Hea
+pDumpPath=/tmp/jvmlog/jvm.hprof -XX:+PrintClassHistogram -Xloggc:/tmp/gc.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC -cp bin/bootstrap.jar cn.citic21.start
+up.Bootstrap start > /dev/null  2>&1 &
 
-#JAVA_GC="-XX:+DisableExplicitGC -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/jvmlog/jvm.hprof -XX:+PrintClassHistogram -Xloggc:/tmp/gc.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC"
+#JAVA_GC="-XX:+DisableExplicitGC -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/jvmlog/jvm.hprof -XX:+PrintClassHistogram -Xloggc:/tmp/gc.log -XX:+PrintGCDetails -XX:+Pri
+ntGCTimeStamps -XX:+PrintHeapAtGC"
 
 JAVA_OPTS="-server -Xss256k -Xmn500m -Xms1g -Xmx1g -XX:PermSize=64m -XX:MaxPermSize=64m -Djava.net.preferIPv4Stack=true -Dfile.encoding=UTF-8 -XX:+UseConcMarkSweepGC"
-#JMX_OPTS="-Djava.rmi.server.hostname=watchserver -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
+#JMX_OPTS="-Djava.rmi.server.hostname=watchserver -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=f
+alse"
 #JPDA_OPTS="-agentlib:jdwp=transport=dt_socket,address=8888,server=y,suspend=y"
 
-LOGGING_CONFIG="-Djava.util.logging.config.file=config/logging.properties"
+LOGGING_CONFIG="-Djava.util.logging.config.file=$LAUNCHER_HOME/config/logging.properties"
 
-CLASSPATH="bin/bootstrap.jar"
+CLASSPATH="$LAUNCHER_HOME/bin/bootstrap.jar"
 
-LAUNCHER_OUT="logs/launcher.out"
+LAUNCHER_OUT="$LAUNCHER_HOME/logs/launcher.out"
 
 nohup java $JAVA_OPTS $JMX_OPTS $JPDA_OPTS $LOGGING_CONFIG -Dlauncher.home=$LAUNCHER_HOME -classpath $CLASSPATH com.dinstone.launcher.Launcher start > $LAUNCHER_OUT 2>&1 &
--- src/script/stop.sh
+++ src/script/stop.sh
@@ -2,6 +2,6 @@
 
 LAUNCHER_HOME=$(cd `dirname $0`; pwd)
 
-LOGGING_CONFIG="-Djava.util.logging.config.file=config/logging.properties"
-CLASSPATH="bin/bootstrap.jar"
+LOGGING_CONFIG="-Djava.util.logging.config.file=$LAUNCHER_HOME/config/logging.properties"
+CLASSPATH="$LAUNCHER_HOME/bin/bootstrap.jar"
 java "$LOGGING_CONFIG" -Dlauncher.home=$LAUNCHER_HOME -classpath "$CLASSPATH" com.dinstone.launcher.Launcher stop
