fdb390fc314d02fafff640c56a3ba855d8e55173
dinstone <dinstone@dinstone-PC>
2015-07-03 17:04:36 +0800

    config stop shell

--- src/script/stop.sh
+++ src/script/stop.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+LAUNCHER_HOME=$(cd `dirname $0`; pwd)
+
 LOGGING_CONFIG="-Djava.util.logging.config.file=config/logging.properties"
 CLASSPATH="bin/bootstrap.jar"
-java "$LOGGING_CONFIG" -classpath "$CLASSPATH" com.dinstone.launcher.Launcher stop
+java "$LOGGING_CONFIG" -Dlauncher.home=$LAUNCHER_HOME -classpath "$CLASSPATH" com.dinstone.launcher.Launcher stop
